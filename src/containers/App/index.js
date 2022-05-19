16ff37eb5b3111c87142e20ae58440908b8b714a
K4llB4ck <yefersonrocha@hotmail.com>
2022-05-18 23:21:29 -0500

    Readme Modificado

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Enrouter from '../../Routes/index';
 import { useSelector, useDispatch } from 'react-redux';
 import { authenticate } from '../../slices/auth.slice';
@@ -8,7 +8,7 @@ const App = () => {
     const { authenticated } = useSelector(state => state.auth);
     const dispatch = useDispatch();
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         //Comprobar la sesión del usuario
         const token = localStorage.getItem('auth');
         if (token) {
